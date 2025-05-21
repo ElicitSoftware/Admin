016360be9cfb8d0f8ee3f147fa9ffd648e7b8cb0
Matthew Demerath <m.demerath@elicitsoftware.com>
2025-05-21 10:43:33 -0400

    Fixed the interaction with the departments combobox. if All Departments is slected it is the only selected item

--- src/main/java/com/elicitsoftware/admin/flow/MainView.java
+++ src/main/java/com/elicitsoftware/admin/flow/MainView.java
@@ -12,26 +12,32 @@ package com.elicitsoftware.admin.flow;
  */
 
 import com.elicitsoftware.model.Department;
-import com.elicitsoftware.model.Subject;
+import com.elicitsoftware.model.Status;
 import com.elicitsoftware.model.User;
 import com.vaadin.flow.component.Direction;
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.MultiSelectComboBox;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.H5;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.HasDynamicTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.quarkus.annotation.UIScoped;
 import io.quarkus.security.identity.SecurityIdentity;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.security.RolesAllowed;
-import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
-import jakarta.persistence.Transient;
+import jakarta.persistence.Query;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * The `MainView` class represents the main view of the application, providing the user interface
@@ -56,7 +62,7 @@ import java.util.List;
  */
 @Route(value = "", layout = MainLayout.class)
 @RolesAllowed("user")
-public class MainView extends VerticalLayout implements HasDynamicTitle {
+class MainView extends VerticalLayout implements HasDynamicTitle {
 
     @Inject
     SecurityIdentity identity;
@@ -64,8 +70,9 @@ public class MainView extends VerticalLayout implements HasDynamicTitle {
     @Inject
     UiSessionLogin uiSessionLogin;
 
-    @Transient
     User user;
+
+    Grid<Status> subjectGrid;
 
     @PostConstruct
     public void init() {
@@ -82,50 +89,161 @@ public class MainView extends VerticalLayout implements HasDynamicTitle {
             } else {
                 ui.setDirection(Direction.LEFT_TO_RIGHT);
             }
-
-            this.setAlignItems(Alignment.CENTER);
-            if (identity.hasRole("admin")) {
-                add(new Paragraph("You are also an admin! You can navigate to the admin view either via sidebar or the button below."));
-            } else {
-                add(new Paragraph("You are not an admin. If you try clicking the button below the view won't be shown (behaviour depends a bit if on dev mode (shows known views) or on production (empty screen/404)."));
-            }
         }
+        add(new H5("Subject search"));
+        createSearchBar();
         createRespondentsTable();
+    }
+
+    private void createSearchBar() {
+        HorizontalLayout searchBar = new HorizontalLayout();
+        searchBar.setPadding(true);
+        searchBar.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        searchBar.setWidth("100%");
+        searchBar.setSpacing(true);
+
+        //Add input fields
+        MultiSelectComboBox<Department> departmentComboBox = getDepartmentComboBox();
+        searchBar.add(departmentComboBox);
+
+        TextField firstNameField = new TextField("First name");
+        searchBar.add(firstNameField);
+
+        TextField lastNameField = new TextField("Last name");
+        searchBar.add(lastNameField);
+
+        TextField email = new TextField("Email");
+        searchBar.add(email);
+
+        TextField phone = new TextField("Phone");
+        searchBar.add(phone);
+
+        Button searchButton = new Button("Search");
+        searchButton.addClickListener(e -> {
+            populateSubjectGrid(getSelectedDepartmentIds(departmentComboBox), firstNameField.getValue(), lastNameField.getValue(), email.getValue(), phone.getValue());
+        });
+        searchBar.add(searchButton);
+        add(searchBar);
     }
 
     private void createRespondentsTable() {
         VerticalLayout respondentsLayout = new VerticalLayout();
         respondentsLayout.setSizeFull();
-
-        //If there are more than one department give the user a dropbox to choose one or all.
-        respondentsLayout.add(getDepartmentComboBox());
         add(respondentsLayout);
         add(getSubjectGrid());
     }
 
-    private MultiSelectComboBox getDepartmentComboBox() {
-        MultiSelectComboBox<Department> departmentComboBox = new MultiSelectComboBox<>("Deparments");
-        List<Department> departments = Department.listAll();
-        departmentComboBox.setItems(user.departments);
+    private MultiSelectComboBox<Department> getDepartmentComboBox() {
+
+        MultiSelectComboBox<Department> departmentComboBox = new MultiSelectComboBox<>("Deparment(s)");
+
+        // Create "All Departments" entry
+        Department allDepartments = new Department();
+        allDepartments.name = "All Departments";
+        allDepartments.id = -1; // Use a special ID or flag to identify this entry
+
+        // Combine "All Departments" with user's departments
+        List<Department> departmentsWithAll = new ArrayList<>();
+        departmentsWithAll.add(allDepartments);
+        departmentsWithAll.addAll(user.departments);
+        departmentComboBox.setItems(departmentsWithAll);
+
         departmentComboBox.setItemLabelGenerator(Department::getName);
+        departmentComboBox.setRequiredIndicatorVisible(true);
+
+        // Set "All Departments" as the default
+        departmentComboBox.setValue(Set.of(allDepartments));
+
+        // Add ValueChangeListener
+        departmentComboBox.addValueChangeListener(event -> {
+
+            Set<Department> oldValues = event.getOldValue();
+            Set<Department> newValues = event.getValue();
+            //If the oldValues did not have allDepartments but the newValues does
+            if (oldValues.stream().noneMatch(od -> od.id == -1)
+                    && newValues.stream().anyMatch(d -> d.id == -1)) {
+                // If "All Departments" is selected, set only it as selected
+                departmentComboBox.setValue(Set.of(allDepartments));
+            //If the newValues only has allDepartments
+            } else if(newValues.size() == 1 && newValues.contains(allDepartments)) {
+                departmentComboBox.setValue(Set.of(allDepartments));
+            }else {
+                // Optionally, you can de-select "All Departments"
+                Set<Department> filtered = newValues.stream()
+                        .filter(d -> d.id != -1)
+                        .collect(Collectors.toSet());
+                departmentComboBox.setValue(filtered);
+            }
+        });
+
         return departmentComboBox;
     }
 
-    private Grid<Subject> getSubjectGrid() {
-        Grid<Subject> grid = new Grid<>(Subject.class, false);
-        grid.addColumn(Subject::getFirstName).setHeader("First name");
-        grid.addColumn(Subject::getLastName).setHeader("Last name");
-        grid.addColumn(Subject::getEmail).setHeader("Email");
-        grid.addColumn(Subject::getPhone).setHeader("Phone");
+    private Grid<Status> getSubjectGrid() {
+        subjectGrid = new Grid<>(Status.class, true);
+//        subjectGrid.addColumn(Status::getToken).setHeader("Token");
+//        subjectGrid.addColumn(Status::getDepartmentName).setHeader("Department");
+//        subjectGrid.addColumn(Status::getFirstName).setHeader("First name");
+//        subjectGrid.addColumn(Status::getMiddleName).setHeader("Middle name");
+//        subjectGrid.addColumn(Status::getLastName).setHeader("Last name");
+//        subjectGrid.addColumn(Status::getEmail).setHeader("Email");
+//        subjectGrid.addColumn(Status::getPhone).setHeader("Phone");
+//        subjectGrid.addColumn(Status::getStatus).setHeader("Status");
+        return subjectGrid;
+    }
 
-        List<Subject> subjects = Subject.listAll();
-        grid.setItems(subjects);
+    private void populateSubjectGrid(String departments, String firstName, String lastName, String email, String phone) {
+        String sql = getStatusSQL(departments, firstName, lastName, email, phone);
+        Query query = Status.getEntityManager().createNativeQuery(sql, Status.class);
+        List<Status> statusList = query.getResultList();
+        subjectGrid.setItems(statusList);
+    }
 
-        return grid;
-    };
+    private String getStatusSQL(String departments, String firstName, String lastName, String email, String phone) {
+        String sql = """
+                 SELECT
+                    s.id,
+                    s.survey_id,
+                 	s.firstname,
+                	s.lastname,
+                	s.dob,
+                	s.email,
+                	s.middlename,
+                	s.phone,
+                	s.xid,
+                	s.created_dt,
+                	s.department_name,
+                    s.token,
+                    s.status
+                    FROM survey.status s
+                    WHERE s.department_id in (
+                """;
+        sql += departments + ")";
+        return sql;
+    }
+
+    private String getSelectedDepartmentIds(MultiSelectComboBox<Department> departmentComboBox) {
+        String ids = "";
+        Set<Department> selecteDepartments = departmentComboBox.getSelectedItems();
+        if (selecteDepartments.isEmpty()) {
+            Notification.show("Please select one or more departments", 3000, Notification.Position.MIDDLE);
+        } else {
+            for (Department department : selecteDepartments) {
+                if (department.id == -1) {
+                    for (Department d : user.departments) {
+                        ids += d.id + ",";
+                    }
+                    break;
+                }
+                ids += department.id + ",";
+            }
+            ids = ids.substring(0, ids.length() - 1);
+        }
+        return ids;
+    }
 
     @Override
     public String getPageTitle() {
-        return "";
+        return "Elicit Administration";
     }
 }
