94ac92be7e75a7812841c67cb8abadc117d5b976
Matthew Demerath <mdemerat@med.umich.edu>
2025-11-21 08:43:14 -0500

    Removed unused or not needed commands

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -2,5 +2,4 @@ echo "Set java to 21"
 jenv local 21.0.2
 echo "build Admin"
 
-./mvnw clean package -Dquarkus.profile=docker -Pproduction -Dquarkus.container-image.build=true
-# ./mvnw clean package -Dmaven.test.skip=true -Dnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -Dquarkus.profile=docker
+./mvnw clean package -Dmaven.test.skip=true -Dquarkus.profile=docker -Pproduction
