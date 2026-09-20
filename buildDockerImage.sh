424176c1186c6dc4850f230971583805d163ff1f
Matthew Demerath <m.demerath@elicitsoftware.com>
2026-09-20 08:06:47 -0400

    Run the tests during the image build
    Drop -Dmaven.test.skip=true from the commented native build line in
    buildDockerImage.sh, and remove %docker.maven.test.skip=true from
    application.properties: maven.test.skip is a Maven property, so that line
    never had any effect.

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -4,4 +4,4 @@ export JAVA_HOME="$(jenv javahome)"
 echo "build Admin"
 
 ./mvnw clean package -Dquarkus.profile=docker -Dquarkus.container-image.build=true
-# ./mvnw clean package -Dmaven.test.skip=true -Dnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -Dquarkus.profile=docker
+# ./mvnw clean package -Dnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -Dquarkus.profile=docker
